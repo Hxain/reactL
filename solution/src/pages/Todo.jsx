d9611ba061f445516c2689a31300bf4d24763d23
Hxain <ss.hussain.dev@gmail.com>
2025-02-09 23:09:49 +0500

    b

--- solution/src/pages/Todo.jsx
+++ solution/src/pages/Todo.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todo({todo}) {
+    const [todoMsg, setTodoMsg]= useState(false)
   return (
     <>
      <form  className="flex">
